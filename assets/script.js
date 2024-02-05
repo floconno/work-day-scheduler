3da7cf7597aa3b09eeeb82e6fe850674f6893995
floconno <floconno@asu.edu>
2024-02-05 16:54:50 -0700

    added date to header

--- assets/script.js
+++ assets/script.js
@@ -17,11 +17,28 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+    $(".time-block").each(function () {
+      // TODO: Add code to get any user input that was saved in localStorage and set
+      // the values of the corresponding textarea elements. HINT: How can the id
+      // attribute of each time-block be used to do this?
+
+
+      var hour = +this.id.split("-").pop();
+      $(this).children
+      //Some logic
+      //Past, present, future
+      $(this).addClass("past");
+    });
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    var today = dayjs(); 
+    var dayWeek = today.format('dddd, MMMM DD[th]');
+    $('#currentDay').text(dayWeek); 
+
   });
   
+
