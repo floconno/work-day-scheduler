573261d2797bdebefe0909f107f9ad3e66fa2167
floconno <floconno@asu.edu>
2024-02-05 17:05:50 -0700

    got currentHour from dayjs to compare

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,8 @@ $(function () {
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     $(".time-block").each(function () {
+
+      var currentHour = dayjs().hour();
       // TODO: Add code to get any user input that was saved in localStorage and set
       // the values of the corresponding textarea elements. HINT: How can the id
       // attribute of each time-block be used to do this?
@@ -26,14 +28,12 @@ $(function () {
       var hour = +this.id.split("-").pop();
       $(this).children
       //Some logic
+      
       //Past, present, future
       $(this).addClass("past");
+      $(this).addClass("present");
+      $(this).addClass("future");
     });
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
     // TODO: Add code to display the current date in the header of the page.
     var today = dayjs(); 
     var dayWeek = today.format('dddd, MMMM DD[th]');
